903aebf1241abff710192cc7a40051a6c1e2fec8
kccistc <kccistc@DESKTOP-48PJ4ST>
2020-04-28 18:42:12 +0900

    It's the final 13-01

--- SelfStudy/src/Study_workshop/Array13_1.java
+++ SelfStudy/src/Study_workshop/Array13_1.java
@@ -4,17 +4,17 @@ package Study_workshop;
 
 public class Array13_1 {
 	
-	public static int maxValue(int[] arr) {
-		int max =0;
-		max=arr[0];
-		for(int i=0; i<arr.length; i++) {
-			if(max<arr[i])
-				max=arr[i];
+	public static int maxValue(int[] arr) { //return값이 있는 최대값 구하는 메서드 생성
+		int max = arr[0];
+//		for(int i=0; i<arr.length; i++) { 일반적인 for문 대신 enhanced for문 사용
+		for(int e :arr) { //int형 정수 e는 arr이라는 배열을 대상으로 {중괄호} 안에 있는 영역을 반복 실행
+			if(max<e)     //max라는 변수가 e보다 작으면 max에 e를 대입한다.
+				max=e;
 		}
-		return max;
-		
+		return max;      //최대값 출력
 	}
-	public static int minValue(int[] arr) {
+	
+	public static int minValue(int[] arr) { //return값이 있는 최소값 구하는 메서드 생성
 		int min = 0;
 		min=arr[0];
 		for(int j=0; j<arr.length; j++) {
@@ -22,12 +22,9 @@ public class Array13_1 {
 				min=arr[j];
 		}
 		return min;
-		
 	}
 	
-	public static void main(String[] args) {
-		int max =0;
-		int min =0;
+	public static void main(String[] args) { //배열 각각의 인자값 선언.
 		
 		int [] arr = new int [5];
 		arr[0] = 10;
@@ -35,8 +32,6 @@ public class Array13_1 {
 		arr[2] = 135;
 		arr[3] = 14;
 		arr[4] = 25;
-		
-		
 		
 		System.out.println("Maximum:" + maxValue(arr));
 		System.out.println("Minimum" +minValue(arr));
